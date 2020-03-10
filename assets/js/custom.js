7f97bdf3ff6c121ea954d3b3b64e49ee0499c13d
Cool BRG <budhram.gurung01@gmail.com>
2020-03-10 19:25:00 +0530

    Removed unwanted JS

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,51 +4,8 @@ $(function(){
   // Activate wow.js
   new WOW().init();
 
-  // var twoDaysFromNow = (new Date().getTime() / 1000) + (86400 * 2) + 1;
-
-  // // Count Down
-  // var flipdown = new FlipDown(twoDaysFromNow, { theme: 'light' })
-  //   .start()
-  //   .ifEnded(() => {
-  //     console.log('The countdown has ended!');
-  //   });
-
-
-  var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',');
-  $('#nurturing-projects').animateNumber(
-    {
-      number: 21,
-      color: '#fff',
-      numberStep: comma_separator_number_step
-    },
-    {
-      easing: 'swing',
-      duration: 1500
-    }
-  )
-  $('#nearly-complete-projects').animateNumber(
-    {
-      number: 5,
-      color: '#ddd',
-      numberStep: comma_separator_number_step
-    },
-    {
-      easing: 'swing',
-      duration: 1000
-    }
-  )
-
   if (location.pathname == "/" || location.pathname == "/gallery/") {
     $('.gallery a').simpleLightbox();
   } 
 
-  if (location.pathname == "/") {
-    var typed = new Typed(".typed", {
-      strings: ["is with you.", "goal is to develop Nepal economically."],
-      typeSpeed: 40,
-      startDelay: 500,
-      backSpeed: 50
-    });
-  }
 });
-
